54a671ab8b72eb11ca30c44d008ec024ba70bf02
GondekNP <npg@berkeley.edu>
2024-11-27 16:51:03 +0000

    properly rendering solara components - still need to sort out cell rendering, which worked with rainfall model but isnt yet appearing

--- vegetation/app.py
+++ vegetation/app.py
@@ -5,9 +5,13 @@ from mesa_geo.visualization import make_geospace_leaflet
 from patch.model import Vegetation
 from patch.space import VegCell
 
+# BIG_TST_JOTR_BOUNDS = [-116.380920, 33.933106, -116.163940, 34.042419]
+TST_JOTR_BOUNDS = [-116.380920, 33.933106, -116.360920, 33.935106]
+
 model_params = {
     "num_steps": Slider("total number of steps", 20, 1, 100, 1),
     "export_data": False,
+    "bounds": TST_JOTR_BOUNDS
 }
 
 
@@ -15,8 +19,8 @@ def cell_portrayal(cell: VegCell) -> Tuple[float, float, float, float]:
     return cell.elevation, cell.elevation, cell.elevation, 1
 
 
-jotr_bounds = [-116.380920, 33.933106, -116.163940, 34.042419]
-model = Vegetation(bounds=jotr_bounds)
+model = Vegetation(bounds=TST_JOTR_BOUNDS)
+
 page = SolaraViz(
     model,
     [
