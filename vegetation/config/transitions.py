989532a562c84766e90a7a2b91ea9eda67dd0b36
GondekNP <npg@berkeley.edu>
2024-12-16 21:38:36 +0000

    add missing breeding survival rate

--- vegetation/config/transitions.py
+++ vegetation/config/transitions.py
@@ -3,9 +3,13 @@ JOTR_ADULT_AGE = 15
 JOTR_REPRODUCTIVE_AGE = 30
 JOTR_SEED_DISPERSAL_DISTANCE = 30
 
+# TODO: Refactor to be more like a config
+# This is a temporary solution to get the transition rates to be
+# valid for the JOTR model, but this doesn't scale well - we need this
+# to probably be more abstract and use a config for at least our initial
 
 def get_jotr_emergence_rate(aridity):
-    rate = 0.3 - (aridity / 1000)
+    rate = 2.0 - (aridity / 1000)
     return rate
 
 
@@ -16,6 +20,8 @@ def get_jotr_survival_rate(life_stage, aridity, nurse_indicator):
         rate = 0.8
     if life_stage == 'adult':
         rate = 0.7
+    if life_stage == 'breeding':
+        rate = 0.65
 
     rate = rate - (aridity / 1000)
     if nurse_indicator:
